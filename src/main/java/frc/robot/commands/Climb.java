74dfcab8316a25970993e867325bfd05ef808306
Randy DeNeefe <puzzlekitticatty@gmail.com>
2026-01-13 18:01:43 -0800

    Created Climb command
    i think this is stuff for controlling the climber?

---
+++ src/main/java/frc/robot/commands/Climb.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+//Imports:
+package frc.robot.commands;
+
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.ROBOT.CONTROL_MODE;
+import frc.robot.subsystems.Climber;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class Climb extends Command {
+  // Creates a new Climb Command
+  private final Climber m_climber;
+  private final Voltage m_voltage;
+
+  
+  public Climb(Climber climber, Voltage voltage) {
+    m_climber = climber;
+    m_voltage = voltage;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_climber);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_climber.setControlMode(CONTROL_MODE.OPEN_LOOP);
+    m_climber.setButtonInput(m_voltage.in(Volts) / 12);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -11,15 +11,20 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CAN;
 import frc.robot.Constants.CLIMBER;
+import frc.robot.Constants.ROBOT.CONTROL_MODE;
 
 
 public class Climber extends SubsystemBase {
 
   TalonFX m_climberMotor = new TalonFX(CAN.kClimberMotor); //Update Number if the DeviceID changes.
-  
+
+  // Booleans:
+  private CONTROL_MODE m_controlMode = CONTROL_MODE.OPEN_LOOP;
+  private double m_buttonInput = 0.0;
+
   /** Creates a new Climber. */
   public Climber() {
-    //Configuring the motor for the elevator.
+    //Configuring the motor for the elevator:
     TalonFXConfiguration config = new TalonFXConfiguration();
     config.Slot0.kG = CLIMBER.kG;
     config.Slot0.kV = CLIMBER.kV;
@@ -29,6 +34,15 @@ public class Climber extends SubsystemBase {
     config.Slot0.kD = CLIMBER.kD;
 
     config.Feedback.SensorToMechanismRatio = CLIMBER.gearRatio;
+  }
+
+  //Methods
+  public void setControlMode(CONTROL_MODE controlMode) {
+    m_controlMode = controlMode;
+  }
+
+  public void setButtonInput(double buttonInput) {
+    m_buttonInput = buttonInput;
   }
 
   @Override
