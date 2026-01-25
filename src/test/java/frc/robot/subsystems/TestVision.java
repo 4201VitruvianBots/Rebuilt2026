629392f564744049faf0d8173f60481f64b0562f
Jonathan Dao <jonathandao0@gmail.com>
2026-01-24 20:04:07 -0800

    Updated Vision unit test with mockito

--- src/test/java/frc/robot/subsystems/TestVision.java
+++ src/test/java/frc/robot/subsystems/TestVision.java
@@ -1,87 +1,44 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Radians;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
 
 import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveDriveState;
-import com.ctre.phoenix6.swerve.jni.SwerveJNI;
-import edu.wpi.first.hal.HAL;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.generated.TunerConstants;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import utils.TestUtils;
 
+@ExtendWith(MockitoExtension.class)
 public class TestVision {
-  CommandSwerveDrivetrain swerveDrive;
-  Controls controls;
-  Vision vision;
+  @Spy CommandSwerveDrivetrain swerveDrive = TunerConstants.createDrivetrain();
+  Controls controls = new Controls();
+  @Spy Vision vision = new Vision(controls);
 
   @BeforeEach
   void setup() {
-    assert HAL.initialize(500, 0);
-
-    swerveDrive = TunerConstants.createDrivetrain();
-    controls = new Controls();
-    vision = new Vision(controls);
     vision.registerSwerveDrive(swerveDrive);
   }
 
-  @Disabled("Issue with JNI crash")
+  @Test
   public void test_isOnTarget() {
     // Set a fake target. For ease, this is (3,4) for a right-triangle
     Pose2d[] mockRobotToTarget = {Pose2d.kZero, new Pose2d(3, 4, Rotation2d.kZero)};
     TestUtils.setPrivateField(vision, "robotToTarget", mockRobotToTarget);
 
-    // Set the Robot's Rotation. m_cachedState is an inherited value from CTRE's class that contains
-    // the values we want from swerveDrive.getState()
-    //        TestUtils.setPrivateField(swerveDrive, "m_cachedState", new SwerveDriveState());
-    SwerveDriveState swerveDriveState =
-        (SwerveDriveState) TestUtils.getPrivateObject(swerveDrive, "m_cachedState");
-    SwerveJNI jni = (SwerveJNI) TestUtils.getPrivateObject(swerveDrive, "m_jni");
-    jni.driveState = new SwerveJNI.DriveState();
-    jni.driveState.PoseTheta = Degrees.of(52).in(Radians);
-    //        SwerveDriveState swerveDriveState = new SwerveDriveState();
-    //        jni.ModuleStates = new SwerveModuleState[] {
-    //                new SwerveModuleState(),
-    //                new SwerveModuleState(),
-    //                new SwerveModuleState(),
-    //                new SwerveModuleState()
-    //        };
-    //        jni.ModuleTargets = new SwerveModuleState[] {
-    //                new SwerveModuleState(),
-    //                new SwerveModuleState(),
-    //                new SwerveModuleState(),
-    //                new SwerveModuleState()
-    //        };
-    //        jni.ModulePositions = new SwerveModulePosition[] {
-    //                new SwerveModulePosition(),
-    //                new SwerveModulePosition(),
-    //                new SwerveModulePosition(),
-    //                new SwerveModulePosition()
-    //        };
-    //        jni.Pose = new Pose2d(0, 0, Rotation2d.fromDegrees(52));
-    //        TestUtils.setPrivateField(swerveDrive, "m_cachedState", swerveDriveState);
+    // Set the robot's position for this test
+    SwerveDriveState swerveDriveState = new SwerveDriveState();
+    swerveDriveState.Pose = new Pose2d(0, 0, Rotation2d.fromDegrees(53));
+    // Use mockito to have the swerveDrive use our fake position
+    doReturn(swerveDriveState).when(swerveDrive).getState();
 
-    Pose2d[] targetAngle = (Pose2d[]) TestUtils.getPrivateObject(vision, "robotToTarget");
-    // System.out.println("Calculated Angle Delta: " + vision.getTranslationDelta().in(Degrees));
     System.out.println("Robot Angle: " + swerveDrive.getState().Pose.getRotation().getDegrees());
 
     assertTrue(vision.isOnTarget());
-  }
-
-  @Test
-  public void test_isOnTarget2() {
-    Pose2d mockRobotPose = new Pose2d(0, 0, Rotation2d.fromDegrees(53));
-    Pose2d mockTargetPosition = new Pose2d(3, 4, Rotation2d.kZero);
-
-    // Update this function
-    var angleDelta = mockRobotPose.getRotation().minus(mockRobotPose.getRotation());
-
-    System.out.println("Calculated Angle Delta: " + angleDelta.getDegrees());
-    assertTrue(angleDelta.getMeasure().lt(Degrees.of(1)));
   }
 }
