424bedf4dc8f699920334bfdca3777c0cd137e2f
Gavin P <gavinjpeeler@gmail.com>
2026-01-25 17:10:41 -0800

    Added indexer and uptake

--- src/main/java/frc/robot/simulation/Robot2d.java
+++ src/main/java/frc/robot/simulation/Robot2d.java
@@ -36,7 +36,7 @@ public class Robot2d extends SubsystemBase {
   private static final DistanceUnit LineWidthInches = derive(Units.Inches).splitInto(12).named("LineWidthInches").symbol("lw in").make();
   
   private final Distance robotCanvasX = Pixels.of(1170);
-  private final Distance robotCanvasY = Pixels.of(818);
+  private final Distance robotCanvasY = Pixels.of(1170);
   
   private final Mechanism2d m_robot = new Mechanism2d(robotCanvasX.in(Inches), robotCanvasY.in(Inches));
   
@@ -47,30 +47,62 @@ public class Robot2d extends SubsystemBase {
   private final MechanismRoot2d m_chassisRoot =
     m_robot.getRoot("chassisRoot", chassisRootX.in(Inches), chassisRootY.in(Inches));
   private final MechanismLigament2d m_chassis = 
-    new MechanismLigament2d("chassis",
+    m_chassisRoot.append(new MechanismLigament2d("chassis",
     Inches.of(30.5).in(Inches), // Full width including bumpers
- 0);
+ 0));
   
+  // Intake
   private final MechanismRoot2d m_intakeRoot =
     m_robot.getRoot("intakeRoot", 
      chassisRootX.plus(Inches.of(10.588)).in(Inches), // Distance from edge of bumper to center of intake pivot
      chassisRootY.plus(Inches.of(3.5)).in(Inches)); // Intake is ~3.5 inches above center of bumpers
   private final Arm2d m_intakePivot =
     new Arm2d(new Arm2dConfig(
-        "intakePivot",
+        "Intake Pivot",
         new Color8Bit(255, 255, 255),
         Degrees.of(159), // Angle between chassis and first segment of intake when extended
         Inches.of(10.735) // Length of first segment of intake
     ).withLineWidth(Inches.of(1.465).in(LineWidthInches)), // Width of the intake pivot arm at its thinnest point
     m_intakeRoot);
+  
+  // Indexer
+  private final MechanismRoot2d m_indexerRoot =
+    m_robot.getRoot("indexerRoot",
+     chassisRootX.plus(Inches.of(13.4)).in(Inches), // Distance from edge of bumper to furthest left indexer roller
+     chassisRootY.plus(Inches.of(2.25)).in(Inches)); // Indexer is ~2.25 inches above center of bumpers
+  private final MechanismLigament2d m_indexer =
+    m_indexerRoot.append(new MechanismLigament2d("Indexer",
+    Inches.of(9.6).in(Inches), // Length of the indexer from first roller to last roller
+    170)); // Angle of the indexer relative to the chassis
+  
+  // Uptake
+  private final MechanismRoot2d m_uptakeRoot =
+    m_robot.getRoot("uptakeRoot",
+     chassisRootX.plus(Inches.of(18.725)).in(Inches), // Distance from edge of bumper to center of thinnest part of uptake
+     chassisRootY.plus(Inches.of(2.25)).in(Inches)); // Indexer is ~2.25 inches above center of bumpers
+  private final MechanismLigament2d m_uptake =
+    m_uptakeRoot.append(new MechanismLigament2d("Uptake",
+    Inches.of(18.5).in(Inches), // Height of the uptake
+    90)); // Straight up
     
+  // Flywheel
+  
+  // Shooter Hood
+  
+  // Climber
+  
+  // TODO: Add hopper, Vision, LEDs?
+  
   /** Map of subsystems for Robot2d to update */
   private final Map<String, Subsystem> m_subsystemMap = new HashMap<>();
   
   /** Creates a new Robot2d. */
   public Robot2d() {
-    m_chassis.setLineWeight(Inches.of(4.5).in(LineWidthInches));
-    m_chassisRoot.append(m_chassis);
+    m_chassis.setLineWeight(Inches.of(4.5).in(LineWidthInches)); // Bumpers are 4.5 inches thick
+    m_indexer.setLineWeight(Inches.of(1.25).in(LineWidthInches)); // Diameter of the indexer rollers is 1.25 inches
+    m_indexer.setColor(new Color8Bit(255, 200, 0)); // Yellow color for indexer
+    m_uptake.setLineWeight(Inches.of(6.65).in(LineWidthInches)); // Width of the uptake at its thinnest point
+    m_uptake.setColor(new Color8Bit(0, 255, 0)); // Green color for uptake
     
     if (RobotBase.isSimulation()) {
       SmartDashboard.putData("Robot2d", m_robot);
