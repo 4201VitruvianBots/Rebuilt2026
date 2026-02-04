56854991bf8567ca1dfa9a3cabbe718e45085959
Romeo <romeopearson30@gmail.com>
2026-02-03 17:17:57 -0800

    WIP auto align port over

---
+++ src/main/java/frc/robot/commands/swerve/DriveToTarget.java
@@ -0,0 +1,102 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.swerve;
+
+import com.pathplanner.lib.commands.FollowPathCommand;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.IdealStartingState;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.SWERVE;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Controls;
+import frc.robot.subsystems.Vision;
+import java.util.List;
+import java.util.Set;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class DriveToTarget {
+  private final CommandSwerveDrivetrain m_swerveDrive;
+  private final Vision m_vision;
+  private final Controls m_controls;
+  private final StructPublisher<Pose2d> desiredTargetPublisher =
+      NetworkTableInstance.getDefault()
+          .getTable("Vision")
+          .getStructTopic("Desired Target", Pose2d.struct)
+          .publish();
+
+  /** Creates a new DriveToBranch. */
+  public DriveToTarget(CommandSwerveDrivetrain swerveDrivetrain, Vision vision, Controls controls) {
+    m_swerveDrive = swerveDrivetrain;
+    m_vision = vision;
+    m_controls = controls;
+  }
+
+//   public Command generateCommand(boolean useLeft) {
+//     return Commands.defer(
+//         () -> {
+//           // figure out where we need to drive to
+//           if (m_controls.isGamePieceCoral()) {
+//             m_vision.setLeftTarget(useLeft);
+//           }
+//           m_vision.updateNearestScoringTarget();
+//           var targetPose = m_vision.getNearestTargetPose();
+
+//           // publish the position we want to drive to
+//           desiredTargetPublisher.accept(targetPose);
+
+//           // generate a path to the desired position
+//           return getPathToWaypoint(targetPose);
+//         },
+//         Set.of());
+//   }
+
+  private Command getPathToWaypoint(Pose2d targetWaypoint) {
+    List<Waypoint> waypoints =
+        PathPlannerPath.waypointsFromPoses(
+            new Pose2d(
+                m_swerveDrive.getState().Pose.getTranslation(),
+                // TODO: Verify if this midpoint is needed.
+                m_swerveDrive.getPathVelocityHeading(
+                    m_swerveDrive.getState().Speeds, targetWaypoint)),
+            targetWaypoint);
+    // TODO:adjust the threshold if needed
+    if (waypoints.get(0).anchor().getDistance(waypoints.get(1).anchor()) < 0.01) {
+      return Commands.sequence(
+          Commands.print("start position PID loop"),
+          PositionPIDCommand.generateCommand(
+              m_swerveDrive, targetWaypoint, SWERVE.kAlignmentAdjustmentTimeout),
+          Commands.print("end position PID loop"));
+    }
+    PathPlannerPath path =
+        new PathPlannerPath(
+            waypoints,
+            SWERVE.kAutoAlignPathConstraints,
+            new IdealStartingState(
+                m_swerveDrive.getVelocityMagnitude(m_swerveDrive.getState().Speeds),
+                m_swerveDrive.getState().Pose.getRotation()),
+            new GoalEndState(0.0, targetWaypoint.getRotation()));
+
+    return ppHolonomicCommand(path);
+  }
+
+  private Command ppHolonomicCommand(PathPlannerPath path) {
+    return new FollowPathCommand(
+            path,
+            () -> m_swerveDrive.getState().Pose,
+            () -> m_swerveDrive.getState().Speeds,
+            m_swerveDrive::setChassisSpeedsAuto,
+            SWERVE.kDriveController,
+            m_swerveDrive.getAutoRobotConfig(),
+            () -> false,
+            m_swerveDrive)
+        .withName("DriveToTarget");
+  }
+}
