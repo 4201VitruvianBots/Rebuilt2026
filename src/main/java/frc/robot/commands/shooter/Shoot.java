3922cab59b697cf4eb5248d0cf821c7358f39949
Nathan S. <nathanael7639@gmail.com>
2026-02-05 11:59:59 -0800

    WIP sotm

--- src/main/java/frc/robot/commands/shooter/Shoot.java
+++ src/main/java/frc/robot/commands/shooter/Shoot.java
@@ -3,14 +3,24 @@ package frc.robot.commands.shooter;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Volts;
 
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
 import edu.wpi.first.math.interpolation.Interpolator;
 import edu.wpi.first.math.interpolation.InverseInterpolator;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.FLYWHEEL.Shot;
+import frc.robot.Constants.SWERVE;
 import frc.robot.constants.FIELD;
+import frc.robot.Constants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Controls;
 import frc.robot.subsystems.Flywheel;
 import frc.robot.subsystems.Vision;
@@ -38,14 +48,27 @@ public class Shoot extends Command {
 
   private final Flywheel m_flywheel;
   private final Vision m_vision;
+  private final CommandSwerveDrivetrain m_swerveDrivetrain;
+  private final DoubleSupplier m_throttleInput;
+  private final DoubleSupplier m_turnInput;
+
+  private Double kTeleP_Theta = 7.4;
+  private Double kTeleD_Theta = 0.3;
+  public static final double kTeleI_Theta = 0.0;
+
+  private PIDController m_PidController =
+    new PIDController(kTeleP_Theta, kTeleI_Theta, kTeleD_Theta);
 
   // private final ShooterHood m_shooterHood;
 
   Translation2d m_goal = new Translation2d();
 
-  public Shoot(Flywheel flywheel, Vision vision) {
+  public Shoot(Flywheel flywheel, Vision vision, CommandSwerveDrivetrain swerveDrive, DoubleSupplier throttleInput, DoubleSupplier turnInput) {
     m_flywheel = flywheel;
     m_vision = vision;
+    m_swerveDrivetrain = swerveDrive;
+    m_throttleInput = throttleInput;
+    m_turnInput = turnInput;
     // m_shooterHood = shooterHood;
 
     addRequirements(flywheel);
@@ -64,9 +87,49 @@ public class Shoot extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    double effectiveDistance = 2.5; // meters
+    Translation2d currentPose = m_swerveDrivetrain.getState().Pose.getTranslation();
+
+    double PositionY = m_swerveDrivetrain.getState().Pose.getY();
+    double PositionX = m_swerveDrivetrain.getState().Pose.getX();
+    double VelocityY = m_swerveDrivetrain.getChassisSpeed().vyMetersPerSecond;
+    double VelocityX = m_swerveDrivetrain.getChassisSpeed().vxMetersPerSecond;
+
+    double AccelerationX = m_swerveDrivetrain.getPigeon2().getAccelerationX().getValueAsDouble();
+    double AccelerationY = m_swerveDrivetrain.getPigeon2().getAccelerationY().getValueAsDouble();
+
+    double VelocityShoot = 9.255586759; // Previously 11.1 m/s
+
+    double virtualGoalX = m_goal.getX() - VelocityShoot * (VelocityX + AccelerationX);
+    double virtualGoalY = m_goal.getY() - VelocityShoot * (VelocityY + AccelerationY);
+
+    SmartDashboard.putNumber("Goal X", virtualGoalX);
+    SmartDashboard.putNumber("Goal Y", virtualGoalY);
+
+    Translation2d movingGoalLocation = new Translation2d(virtualGoalX, virtualGoalY);
+
+    Translation2d toMovingGoal = movingGoalLocation.minus(currentPose);
+
+    double newDist = toMovingGoal.getDistance(new Translation2d());
+
+    double getOffsetAngleDeg =
+        Math.asin((VelocityY * PositionX + VelocityX * PositionY) / (newDist * effectiveDistance));
+
+    var targetDelta = (m_swerveDrivetrain.getState().Pose.getTranslation().minus(m_goal).getAngle());
+
+    // all of the logic for angle is above this Comment
+
     Shot shot = distanceToShotMap.get(m_vision.getDistancetoHub());
     m_flywheel.setRPMOutputFOC(shot.shooterRPM);
     // m_shooterHood.setPosition(shot.hoodPosition);
+
+    m_swerveDrivetrain.setChassisSpeedControl(
+        new ChassisSpeeds(
+            (m_throttleInput.getAsDouble()) * SWERVE.kMaxSpeedMetersPerSecond,
+            (m_turnInput.getAsDouble()) * SWERVE.kMaxSpeedMetersPerSecond,
+            m_PidController.calculate(
+                    m_swerveDrivetrain.getState().Pose.getRotation().getRadians(),
+                    targetDelta.getRadians() + getOffsetAngleDeg)));
   }
 
   // Called once the command ends or is interrupted.
