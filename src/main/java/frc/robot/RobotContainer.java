b9b40100b541543164830e280a3e7922d57a113a
Nathan Johnson <swirlingscudder@gmail.com>
2026-02-06 18:01:22 -0800

    MY NAME IS OZYMANDIAS, KING OF KINGS; LOOK UPON MY WORKS, YE MIGHTY

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -51,9 +52,9 @@ public class RobotContainer {
   private CommandSwerveDrivetrain m_swerveDrive = TunerConstants.createDrivetrain();
 
   private Controls m_controls = new Controls();
-
+  private RobotState state = new RobotState();
   @Logged(name = "Vision", importance = Logged.Importance.INFO)
-  private Vision m_vision = new Vision(m_controls);
+  private Vision m_vision = new Vision(m_controls, state);
 
   private Telemetry m_telemetry = new Telemetry();
 
