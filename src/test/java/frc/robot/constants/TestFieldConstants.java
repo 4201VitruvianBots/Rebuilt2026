e7f3ba69b8d35a4ed99cb59daace2dab2a96adf7
jonathandao0 <9299703+jonathandao0@users.noreply.github.com>
2026-02-09 02:21:41 +0000

    Automatically applied spotless

--- src/test/java/frc/robot/constants/TestFieldConstants.java
+++ src/test/java/frc/robot/constants/TestFieldConstants.java
@@ -6,11 +6,10 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.subsystems.Controls;
-import org.junit.jupiter.api.Test;
-import utils.TestUtils;
-
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.Test;
+import utils.TestUtils;
 
 public class TestFieldConstants {
 
@@ -29,35 +28,37 @@ public class TestFieldConstants {
     FIELD.updateConstants();
 
     Map<Pose2d, FIELD.SECTOR> redSectors = new HashMap<>();
-    redSectors.put(new Pose2d(1,1, Rotation2d.kZero), FIELD.SECTOR.BLUE_LEFT);
-    redSectors.put(new Pose2d(1,8, Rotation2d.kZero), FIELD.SECTOR.BLUE_RIGHT);
-    redSectors.put(new Pose2d(5,1, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_FAR_LEFT);
-    redSectors.put(new Pose2d(5,8, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_FAR_RIGHT);
-    redSectors.put(new Pose2d(9,1, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_NEAR_LEFT);
-    redSectors.put(new Pose2d(9,8, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_NEAR_RIGHT);
-    redSectors.put(new Pose2d(13,1, Rotation2d.kZero), FIELD.SECTOR.RED_LEFT);
-    redSectors.put(new Pose2d(13,8, Rotation2d.kZero), FIELD.SECTOR.RED_RIGHT);
+    redSectors.put(new Pose2d(1, 1, Rotation2d.kZero), FIELD.SECTOR.BLUE_LEFT);
+    redSectors.put(new Pose2d(1, 8, Rotation2d.kZero), FIELD.SECTOR.BLUE_RIGHT);
+    redSectors.put(new Pose2d(5, 1, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_FAR_LEFT);
+    redSectors.put(new Pose2d(5, 8, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_FAR_RIGHT);
+    redSectors.put(new Pose2d(9, 1, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_NEAR_LEFT);
+    redSectors.put(new Pose2d(9, 8, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_NEAR_RIGHT);
+    redSectors.put(new Pose2d(13, 1, Rotation2d.kZero), FIELD.SECTOR.RED_LEFT);
+    redSectors.put(new Pose2d(13, 8, Rotation2d.kZero), FIELD.SECTOR.RED_RIGHT);
 
-    redSectors.forEach((k, v)-> {
-      FIELD.updateCurrentSector(k);
-      assertEquals(v, FIELD.getCurrentSector());
-    });
+    redSectors.forEach(
+        (k, v) -> {
+          FIELD.updateCurrentSector(k);
+          assertEquals(v, FIELD.getCurrentSector());
+        });
 
     // Test Blue Alliance
     TestUtils.setPrivateField(controls, "m_allianceColor", DriverStation.Alliance.Blue);
     Map<Pose2d, FIELD.SECTOR> blueSectors = new HashMap<>();
-    blueSectors.put(new Pose2d(1,1, Rotation2d.kZero), FIELD.SECTOR.BLUE_RIGHT);
-    blueSectors.put(new Pose2d(1,8, Rotation2d.kZero), FIELD.SECTOR.BLUE_LEFT);
-    blueSectors.put(new Pose2d(5,1, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_NEAR_RIGHT);
-    blueSectors.put(new Pose2d(5,8, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_NEAR_LEFT);
-    blueSectors.put(new Pose2d(9,1, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_FAR_RIGHT);
-    blueSectors.put(new Pose2d(9,8, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_FAR_LEFT);
-    blueSectors.put(new Pose2d(13,1, Rotation2d.kZero), FIELD.SECTOR.RED_LEFT);
-    blueSectors.put(new Pose2d(13,8, Rotation2d.kZero), FIELD.SECTOR.RED_RIGHT);
+    blueSectors.put(new Pose2d(1, 1, Rotation2d.kZero), FIELD.SECTOR.BLUE_RIGHT);
+    blueSectors.put(new Pose2d(1, 8, Rotation2d.kZero), FIELD.SECTOR.BLUE_LEFT);
+    blueSectors.put(new Pose2d(5, 1, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_NEAR_RIGHT);
+    blueSectors.put(new Pose2d(5, 8, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_NEAR_LEFT);
+    blueSectors.put(new Pose2d(9, 1, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_FAR_RIGHT);
+    blueSectors.put(new Pose2d(9, 8, Rotation2d.kZero), FIELD.SECTOR.NEUTRAL_FAR_LEFT);
+    blueSectors.put(new Pose2d(13, 1, Rotation2d.kZero), FIELD.SECTOR.RED_LEFT);
+    blueSectors.put(new Pose2d(13, 8, Rotation2d.kZero), FIELD.SECTOR.RED_RIGHT);
 
-    blueSectors.forEach((k, v)-> {
-      FIELD.updateCurrentSector(k);
-      assertEquals(v, FIELD.getCurrentSector());
-    });
+    blueSectors.forEach(
+        (k, v) -> {
+          FIELD.updateCurrentSector(k);
+          assertEquals(v, FIELD.getCurrentSector());
+        });
   }
 }
