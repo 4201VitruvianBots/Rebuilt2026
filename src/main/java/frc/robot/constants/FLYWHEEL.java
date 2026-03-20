2daa087cfff1e56bad87f4aa6a5e25b01dd33249
Nathan S. <nathanael7639@gmail.com>
2026-03-19 19:32:53 -0700

    End of 3/19 auto testing

--- src/main/java/frc/robot/constants/FLYWHEEL.java
+++ src/main/java/frc/robot/constants/FLYWHEEL.java
@@ -17,7 +17,7 @@ public class FLYWHEEL {
   public static final double gearRatio = 32.0 / 24.0; // Placeholder value
   public static final double kInertia = 0.01;
   public static final double kStatorCurrentLimit = 70.0;
-  public static final double kVelocityErrorThreshold = 150.0;
+  public static final double kVelocityErrorThreshold = 100.0;
   public static final double kFuelDragCoefficient =
       0.48; // Estimation based on it's size and relatively smooth shape. TODO: Tune
   public static final double kRumbleStrength = 0.25;
