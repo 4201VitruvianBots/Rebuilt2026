4adb7ded8cbdd9131c0d275fb7a52809c08f7a0a
joaquin rodriguez <jqnoonies@gmail.com>
2026-03-20 16:55:59 -0700

    3/20 4-6PM Pit
    uncomment shoot against hub

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -236,11 +237,11 @@ public class RobotContainer {
 
     m_driverController.rightBumper().whileTrue(m_intake.commandIntakeState(INTAKE_STATE.REVERSING));
 
-    // m_driverController
-    //     .x()
-    //     .whileTrue(
-    //         new ParallelCommandGroup(
-    //             m_flywheel.manualAgainstHubCommand(), m_hood.manualAgainstHubCommand()));
+    m_driverController
+        .x()
+        .whileTrue(
+            new ParallelCommandGroup(
+                m_flywheel.manualAgainstHubCommand(), m_hood.manualAgainstHubCommand()));
 
     m_driverController
         .leftBumper()
