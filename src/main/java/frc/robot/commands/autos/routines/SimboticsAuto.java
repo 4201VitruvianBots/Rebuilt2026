e25f3d2d8386899794ee48fea40b6dcfe3ab7fa4
Nathan S. <nathanael7639@gmail.com>
2026-03-25 12:36:00 -0700

    Improved path stuttering

--- src/main/java/frc/robot/commands/autos/routines/SimboticsAuto.java
+++ src/main/java/frc/robot/commands/autos/routines/SimboticsAuto.java
@@ -20,7 +20,7 @@ public class SimboticsAuto extends Auto {
 
       addCommands(
           m_path1,
-          new AutoShoot(deps, 3.0),
+          new AutoShoot(deps, 2.7),
           m_path2);
     } catch (Exception e) {
       DriverStation.reportError("Failed to load path for Simbotics Auto", e.getStackTrace());
