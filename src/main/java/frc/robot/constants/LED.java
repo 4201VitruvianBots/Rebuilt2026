69a8858f618496444fb2940abe6c178c37121837
joaquin rodriguez <jqnoonies@gmail.com>
2026-03-31 13:29:49 -0700

    Add CAD's EVIL and PERPOSTEROUS and DESTRUCTIVE removal of LEDs
    its changed from 42 to 34
    IT IS EVIL and they are EVIL

--- src/main/java/frc/robot/constants/LED.java
+++ src/main/java/frc/robot/constants/LED.java
@@ -3,7 +3,7 @@ package frc.robot.constants;
 public class LED {
   public static final int kPWMPort = 8;
 
-  public static final int kLEDCount = 42;
+  public static final int kLEDCount = 34;
 
   public enum LED_STATES {
     DISABLED_NOT_READY("disabled_not_ready"),
