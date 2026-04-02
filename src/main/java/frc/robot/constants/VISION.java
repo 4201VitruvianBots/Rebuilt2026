cab2e7dfaca67d5141c1e0c53fcd23ed1f1cfdcc
Jonathan Dao <jonathandao0@gmail.com>
2026-04-02 07:01:47 -0700

    Fixed pose reset after auto, added stddevs

--- src/main/java/frc/robot/constants/VISION.java
+++ src/main/java/frc/robot/constants/VISION.java
@@ -6,11 +6,16 @@ package frc.robot.constants;
 
 import static edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.networktables.*;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
+import frc.team4201.lib.vision.LimelightHelpers;
 
 public final class VISION {
   public enum CAMERA_SERVER {
@@ -83,26 +88,37 @@ public final class VISION {
     private final DoubleSubscriber hbSub;
     private double lastHeartbeat = -1.0;
     private boolean isAlive = false;
+    private final DoubleArraySubscriber stddevsSub;
 
     private final DoublePublisher hbPub;
     private final DoublePublisher estimatedTimestamp;
+    private final DoublePublisher robotTimestamp;
     private final StructPublisher<Pose2d> estimatedPose;
+    private final DoubleArrayPublisher estimatedPoseStdDevs;
     private final IntegerPublisher numTags;
     private final BooleanPublisher megatag2Pose;
     private final BooleanPublisher validPose;
+
+    private LimelightHelpers.PoseEstimate lastGoodEstimate = new LimelightHelpers.PoseEstimate();
+    private Matrix<N3, N1> stdDevs = VecBuilder.fill(0, 0, 0);
 
     public Limelight(CAMERA_SERVER limelight) {
       this.limelight = limelight;
       var ntInst = NetworkTableInstance.getDefault();
       var llSubTable = ntInst.getTable(limelight.name);
       hbSub = llSubTable.getDoubleTopic("hb").subscribe(-1.0);
+      stddevsSub = llSubTable.getDoubleArrayTopic("stddevs").subscribe(new double[12]);
 
       var llPubTable = ntInst.getTable("llTable").getSubTable(limelight.name);
       hbPub = llPubTable.getDoubleTopic("heartbeat").publish();
       estimatedTimestamp = llPubTable.getDoubleTopic("estTimestamp").publish();
       estimatedTimestamp.setDefault(-1);
+      robotTimestamp = llPubTable.getDoubleTopic("robotTimestamp").publish();
+      robotTimestamp.setDefault(-1);
       estimatedPose = llPubTable.getStructTopic("estPose", Pose2d.struct).publish();
       estimatedPose.setDefault(new Pose2d(-1, -1, Rotation2d.kZero));
+      estimatedPoseStdDevs = llPubTable.getDoubleArrayTopic("stddevs").publish();
+      estimatedPoseStdDevs.setDefault(new double[3]);
       numTags = llPubTable.getIntegerTopic("numTags").publish();
       numTags.setDefault(-1);
       megatag2Pose = llPubTable.getBooleanTopic("isMegatag2Pose").publish();
@@ -125,8 +141,16 @@ public final class VISION {
       estimatedTimestamp.set(timestamp);
     }
 
+    public void publishRobotTimestamp(double timestamp) {
+      robotTimestamp.set(timestamp);
+    }
+
     public void publishPose(Pose2d pose) {
       estimatedPose.set(pose);
+    }
+
+    public void publishPoseStdDevs(Matrix<N3, N1> stdDevs) {
+      estimatedPoseStdDevs.set(stdDevs.getData());
     }
 
     public void publishTagCount(int tags) {
@@ -153,6 +177,29 @@ public final class VISION {
       hbPub.set(heartbeat);
 
       return lastHeartbeat;
+    }
+
+    public Matrix<N3, N1> getStdDev(boolean mt2) {
+      var stddevArray = stddevsSub.get();
+      if (!mt2) {
+        stdDevs = VecBuilder.fill(stddevArray[0], stddevArray[1], stddevArray[5]);
+      } else {
+        stdDevs = VecBuilder.fill(stddevArray[6], stddevArray[7], stddevArray[11]);
+      }
+
+      return stdDevs;
+    }
+
+    public Matrix<N3, N1> getStdDev() {
+      return stdDevs;
+    }
+
+    public void setLastGoodEstimate(LimelightHelpers.PoseEstimate estimate) {
+      lastGoodEstimate = estimate;
+    }
+
+    public LimelightHelpers.PoseEstimate getLastGoodEstimate() {
+      return lastGoodEstimate;
     }
 
     public boolean isAlive() {
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.*;
 
+import com.ctre.phoenix6.Utils;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.Logged.Importance;
 import edu.wpi.first.math.VecBuilder;
@@ -40,6 +41,7 @@ public class Vision extends SubsystemBase {
 
   private boolean lockTarget = false;
   private boolean hasInitialPose = false;
+  private boolean matchStarted = false;
 
   // NetworkTables publisher setup
   public final DoubleSubscriber m_kPAutoAlignSubscriber;
@@ -175,19 +177,23 @@ public class Vision extends SubsystemBase {
       limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
     }
 
-    boolean validResult = isPoseValid(limelightName, limelightMeasurement);
+    boolean validResult = isPoseValid(limelight, limelightMeasurement);
 
     // Log Data
     limelight.getHeartbeat();
     limelight.publishValid(validResult);
     if (limelightMeasurement != null) {
       limelight.publishTimestamp(limelightMeasurement.timestampSeconds);
+      limelight.publishRobotTimestamp(Utils.getCurrentTimeSeconds());
       limelight.publishPose(limelightMeasurement.pose);
+      limelight.publishPoseStdDevs(limelight.getStdDev(limelightMeasurement.isMegaTag2));
       limelight.publishTagCount(limelightMeasurement.tagCount);
       limelight.publishMegatag2Pose(limelightMeasurement.isMegaTag2);
     } else {
       limelight.publishTimestamp(-1);
+      limelight.publishRobotTimestamp(-1);
       limelight.publishPose(new Pose2d(-1, -1, Rotation2d.kZero));
+      limelight.publishPoseStdDevs(VecBuilder.fill(0, 0, 0));
       limelight.publishTagCount(-1);
       limelight.publishMegatag2Pose(false);
     }
@@ -195,22 +201,26 @@ public class Vision extends SubsystemBase {
     if (validResult) {
       // Only good updates reach this point, so use them for updating the robot pose
       assert limelightMeasurement != null;
-      m_swerveDriveTrain.addVisionMeasurement(
-          limelightMeasurement.pose, limelightMeasurement.timestampSeconds);
 
       // Reset the Swerve Pose with MegaTag1 if we are disabled
-      if (DriverStation.isDisabled() && !limelightMeasurement.isMegaTag2) {
+      if (DriverStation.isDisabled() && !limelightMeasurement.isMegaTag2 && !matchStarted) {
         m_swerveDriveTrain.resetPose(limelightMeasurement.pose);
+      } else {
+        m_swerveDriveTrain.addVisionMeasurement(
+            limelightMeasurement.pose,
+            limelightMeasurement.timestampSeconds,
+            limelight.getStdDev(limelightMeasurement.isMegaTag2));
       }
     }
 
     return validResult;
   }
 
-  public boolean isPoseValid(String limelightName, LimelightHelpers.PoseEstimate poseEstimate) {
+  public boolean isPoseValid(
+      VISION.Limelight limelight, LimelightHelpers.PoseEstimate poseEstimate) {
     if (poseEstimate == null) {
       if (RobotBase.isReal())
-        DriverStation.reportWarning(limelightName + " is not connected", true);
+        DriverStation.reportWarning(limelight.getName() + " is not connected", true);
       return false;
     } else {
       // Filter out bad AprilTag vision estimates for both MegaTag1 and MegaTag2
@@ -228,19 +238,14 @@ public class Vision extends SubsystemBase {
         if (poseEstimate.tagCount < 2) {
           return false;
         }
-        LimelightHelpers.SetFiducialIDFiltersOverride(limelightName, excludeTrenchTags);
+        LimelightHelpers.SetFiducialIDFiltersOverride(limelight.getName(), excludeTrenchTags);
 
         hasInitialPose = true;
-        // Set Standard Deviations for MegaTag1
-        m_swerveDriveTrain.setVisionMeasurementStdDevs(VecBuilder.fill(.5, .5, 9999999));
       } else {
         // Ignore MegaTag2 updates if the robot is spinning too fast
         if (m_swerveDriveTrain.getGyroYawRate().abs(DegreesPerSecond) > 720.0) {
           return false;
         }
-
-        // Set Standard Deviations for MegaTag2
-        m_swerveDriveTrain.setVisionMeasurementStdDevs(VecBuilder.fill(.4, .4, 9999999));
       }
     }
 
@@ -252,14 +257,19 @@ public class Vision extends SubsystemBase {
     return this.hasInitialPose;
   }
 
-  @Logged(name = "LLL Connected", importance = Logged.Importance.INFO)
-  public boolean lllConnected() {
-    return LLL.isAlive();
+  @Logged(name = "LLF Connected", importance = Logged.Importance.INFO)
+  public boolean llfConnected() {
+    return LLF.isAlive();
   }
 
   @Logged(name = "LLR Connected", importance = Logged.Importance.INFO)
   public boolean llrConnected() {
     return LLR.isAlive();
+  }
+
+  @Logged(name = "LLL Connected", importance = Logged.Importance.INFO)
+  public boolean lllConnected() {
+    return LLL.isAlive();
   }
 
   /** Stop the nearest target from updating when we want to score to avoid target switching */
@@ -287,9 +297,9 @@ public class Vision extends SubsystemBase {
   @Logged(name = "On Target", importance = Logged.Importance.DEBUG)
   public boolean isOnTarget() {
     if (DriverStation.isAutonomous()) {
-        return getAngleToTarget().getDegrees() < 2.0;
+      return getAngleToTarget().getDegrees() < 2.0;
     } else {
-        return getAngleToTarget().getDegrees() < 0.5;
+      return getAngleToTarget().getDegrees() < 0.5;
     }
   }
 
@@ -373,9 +383,10 @@ public class Vision extends SubsystemBase {
       m_localized = lllSuccess && llrSuccess && llfSuccess;
     }
 
-    // if (m_swerveDriveTrain != null) {
-    //   updateAngleToHub();
-    // }
+    // Do this to avoid issues with the brief 'disabled' period between auto and teleop
+    if (DriverStation.isFMSAttached() && DriverStation.isAutonomous() && matchStarted) {
+      matchStarted = true;
+    }
   }
 
   @Override
