1eea206a95e167efbdb187c7b0a10a338f9158b1
Nathan S. <nathanael7639@gmail.com>
2026-04-03 13:17:25 -0700

    Pre-Q16
    Raise specific rpm values back up, just the ones further from the hub

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -60,16 +60,16 @@ public class Shoot extends Command {
     distanceToShotMap.put(
         Meters.of(2.0749597158415), new Shot(RPM.of(1540 - 10), Degrees.of(0), 1.1)); // Tuned
     distanceToShotMap.put(
-        Meters.of(2.31209), new Shot(RPM.of(1553 - 10), Degrees.of(0.4), 1.1)); // Tuned
+        Meters.of(2.31209), new Shot(RPM.of(1553), Degrees.of(0.4), 1.1)); // Tuned
     distanceToShotMap.put(
-        Meters.of(2.5916617555783), new Shot(RPM.of(1570 - 10), Degrees.of(2.2), 1.1)); // Tuned
+        Meters.of(2.5916617555783), new Shot(RPM.of(1570), Degrees.of(2.2), 1.1)); // Tuned
     distanceToShotMap.put(
-        Meters.of(3.152353828396097), new Shot(RPM.of(1648 - 10), Degrees.of(3.5), 1.1)); // Tuned
+        Meters.of(3.152353828396097), new Shot(RPM.of(1648), Degrees.of(3.5), 1.1)); // Tuned
     distanceToShotMap.put(
-        Meters.of(3.97453), new Shot(RPM.of(1764.3 - 10), Degrees.of(6.234), 1.1)); // Tuned
-    distanceToShotMap.put(Meters.of(4.2697), new Shot(RPM.of(1764.3 - 10), Degrees.of(8), 1.16));
+        Meters.of(3.97453), new Shot(RPM.of(1764.3), Degrees.of(6.234), 1.1)); // Tuned
+    distanceToShotMap.put(Meters.of(4.2697), new Shot(RPM.of(1764.3), Degrees.of(8), 1.16));
     distanceToShotMap.put(
-        Meters.of(5.44820580711993), new Shot(RPM.of(1945 - 10), Degrees.of(14), 1.16));
+        Meters.of(5.44820580711993), new Shot(RPM.of(1945), Degrees.of(14), 1.16));
   }
 
   private final Vision m_vision;
