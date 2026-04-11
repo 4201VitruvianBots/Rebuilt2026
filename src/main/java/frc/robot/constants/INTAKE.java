3bce975c19a7c7918629918d349f030f3a063b93
Nathan S. <nathanael7639@gmail.com>
2026-04-11 09:53:32 -0700

    Pre-Q45

--- src/main/java/frc/robot/constants/INTAKE.java
+++ src/main/java/frc/robot/constants/INTAKE.java
@@ -78,7 +78,7 @@ public class INTAKE {
     public static final double kStatorCurrentLimit = 65.0;
 
     public static final Angle minAngle = Degrees.of(0.0);
-    public static final Angle maxAngle = Degrees.of(105.0);
+    public static final Angle maxAngle = Degrees.of(102.0);
     public static final Angle startingAngle = maxAngle;
     public static final GravityTypeValue K_GRAVITY_TYPE_VALUE =
         GravityTypeValue
@@ -89,7 +89,7 @@ public class INTAKE {
         Inches.of(13.897040); /* Almost completely made up :P */
     public static final Mass mass = Pounds.of(2); // TODO: Consult CAD
 
-    public static final double encoderOffset = -0.7470703125;
+    public static final double encoderOffset = -0.7646484375;
     public static final SensorDirectionValue encoderDirection =
         SensorDirectionValue.CounterClockwise_Positive;
     public static final double kAbsoluteSensorDiscontinuityPoint = 1;
@@ -98,7 +98,7 @@ public class INTAKE {
 
     public enum PIVOT_SETPOINT {
       STOWED(Degrees.of(0.0)),
-      INTAKING(Degrees.of(101.542969)),
+      INTAKING(Degrees.of(101.718750)),
       JOSTLING(Degrees.of(20.0));
 
       private final Angle angle;
