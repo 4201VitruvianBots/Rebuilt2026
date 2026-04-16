75a3faef408e09a2bb841b73e6b28e0a9ffb9289
joaquin rodriguez <jqnoonies@gmail.com>
2026-04-15 19:52:49 -0700

    Add smartdashboard widget checking if in neutral zone
    its lwk like for debugging purposes but could be used for other things possibly?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -443,6 +443,7 @@ public class RobotContainer {
 
   public void robotPeriodic() {
     FIELD.updateCurrentSector(m_swerveDrive.getState().Pose);
+    SmartDashboard.putBoolean("In Neutral Zone?", m_vision.isInNeutralSector());
   }
 
   public void initializeFuelSim() {
